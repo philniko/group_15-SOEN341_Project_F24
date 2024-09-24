e77b6604a6daa80e522ab6316ca734d4a0e26a03
CharlesP <charles.partous@gmail.com>
2024-09-24 16:09:50 -0400

    Create frontend component for instructor dashboard and implement routing in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Register from './Register'
 import Login from './Login'
+import DashboardInstructor from './DashboardInstructor'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/dashboard-instructor' element={<DashboardInstructor />}></Route>
       </Routes>
     </BrowserRouter>
   )
