cd09ad59e7770490b1b345a7214409a583667214
zach <zacharycorber@yahoo.ca>
2024-09-26 16:31:11 -0400

    implemented frontend login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Register from './Register'
+import Login from './Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/register' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   )
