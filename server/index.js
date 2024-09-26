3478a52611796a22f3f19df4bedb7f7ed732fcc7
Philippe Nikolov <pnikolov@datexcorp.com>
2024-09-25 20:50:49 -0400

    initial commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import UserModel from "./models/User.js";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
@@ -13,26 +14,33 @@ mongoose.connect("mongodb://localhost:27017/user");
 //handling register request
 app.post("/register", (req, res) => {
   if (!req.body.email) {
-    res.json("Please enter an email");
+    res.status(400).json("Please enter an email");
   } else if (!req.body.firstName) {
-    res.json("Please enter a first name");
+    res.status(400).json("Please enter a first name");
   } else if (!req.body.lastName) {
-    res.json("Please enter a last name");
+    res.status(400).json("Please enter a last name");
   } else if (!req.body.password) {
-    res.json("Please enter a password");
+    res.status(400).json("Please enter a password");
   } else if (!req.body.role) {
-    res.json("Please choose a role");
+    res.status(400).json("Please choose a role");
   } else {
     //every field is filled
-    UserModel.findOne({ username: req.body.username }).then((user) => {
+    UserModel.findOne({ email: req.body.email }).then((user) => {
       if (user) {
-        res.json("Username is already taken");
+        return res
+          .status(409)
+          .json("An account already exists with this email");
       } else {
         //every field is valid: proceeding with user creation
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        req.body.password = hash;
         req.body.teams = [];
         UserModel.create(req.body)
-          .then((user) => res.json(user))
-          .catch((err) => res.json(err));
+          .then((user) => res.status(201).json(user))
+          .catch((err) =>
+            res.status(500).json("An error occurred during registration")
+          );
       }
     });
   }
@@ -40,13 +48,19 @@ app.post("/register", (req, res) => {
 
 //handling login request
 app.post("/login", (req, res) => {
-  UserModel.findOne({ username: req.body.username }).then((user) => {
+  UserModel.findOne({ email: req.body.email}).then((user) => {
     if (!user) {
-      res.json("User does not exist");
-    } else if (req.body.password != user.password) {
-      res.json("Password is invalid");
+      res.status(404).json("User does not exist");
     } else {
-      res.json("Login success!");
+      const validPassword = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+      if (validPassword) {
+        res.status(200).json("Login successful");
+      } else {
+        res.status(401).json("Invalid password");
+      }
     }
   });
 });
