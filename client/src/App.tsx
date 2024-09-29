6dfd8802d672ec2f202fd4677e9ca6cf8ed854b6
Carlucc10 <carlo.ramadori25@gmail.com>
2024-09-29 18:56:34 -0400

    Created the front-end of the student dashboard. Just the skeleton for now, as more backend is created, more front-end will be implemented as well

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,37 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Register from './Register'
 import Login from './Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sidebar from './StudentDashboard/Sidebar.tsx'
+import './StudentDashboard/StudentDashboard.css'
+import Home from './StudentDashboard/Home.tsx'
+import MyGroup from './StudentDashboard/MyGroup.tsx'
+import UserSettings from './StudentDashboard/UserSettings.tsx'
+
 
 function App() {
 
+  const LayoutWithSidebar = () => (
+      <>
+        <Sidebar />
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/mygroup" element={<MyGroup />} />
+          <Route path="/usersettings" element={<UserSettings />} />
+        </Routes>
+      </>
+  );
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Routes with Sidebar */}
+          <Route path="/*" element={<LayoutWithSidebar />} />
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App
