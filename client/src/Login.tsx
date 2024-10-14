53e034aa138a8e5ff21c0fb9b1eb4c9e5eba6281
Ke-Li02 <143147612+Ke-Li02@users.noreply.github.com>
2024-10-13 23:26:45 -0400

    bug fix

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -28,12 +28,8 @@ function Login() {
                 // Store the JWT token in local storage
                 localStorage.setItem('token', data.token);
                 console.log('login data: ', data);
-                if (data.token.role == "instructor") { // Redirect to dashboard/home page
-                    navigate('/instructor/home');
-                }
-                else {
-                    navigate('/student/home');
-                }
+                navigate('/' + data.role + '/home'); // Redirect to dashboard/home page
+
             } else {
                 setError(data);
             }
