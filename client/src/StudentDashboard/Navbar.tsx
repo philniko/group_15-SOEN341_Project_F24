e77440926646da9b5b658b5a066893c082241b7a
Charles Partous <charles.partous@gmail.com>
2024-10-23 14:44:35 -0400

    fix navbar home redirect for student and instructor

--- client/src/StudentDashboard/Navbar.tsx
+++ client/src/StudentDashboard/Navbar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useUser } from '../hooks/UseUser'; //hook to get info about current user
 import './StudentDashboard.css';
 import './Navbar.css';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // Import Link for navigation
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -21,7 +21,8 @@ const Navbar = () => {
         {user ? `${user.firstname}` : 'Welcome'} {/* Display first name if available */}
       </h1>
       <ul className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}>
-        <li><a href="Home">Home</a></li>
+        {/* Use Link for proper internal routing */}
+        <li><Link to="/student/Home">Home</Link></li>
         <li><a href="" onClick={handleLogout}>Logout</a></li>
       </ul>
       <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
