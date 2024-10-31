bc600283dd57d9dc0d20499a8b62cae10b890b0a
Philippe Nikolov <Philippe.nikolov@outlook.com>
2024-10-31 13:14:36 -0400

    initial commit

--- client/src/InstructorDashboard/Group.tsx
+++ client/src/InstructorDashboard/Group.tsx
@@ -1,110 +1,110 @@
-import {useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Group() {
-    let { id } = useParams();
-    const studentEmailInput = useRef<HTMLInputElement>(null);
-    const [studentEmail, setStudentEmail] = useState("");
-    const [students, setStudents] = useState<{_id: string, firstName: string, lastName: string}[]>([]);
-    const [messageType, setMessageType] = useState("");
-    const [message, setMessage] = useState("");
+  const { id } = useParams();
+  const studentEmailInput = useRef<HTMLInputElement>(null);
+  const [studentEmail, setStudentEmail] = useState("");
+  const [students, setStudents] = useState<{ _id: string, firstName: string, lastName: string }[]>([]);
+  const [messageType, setMessageType] = useState("");
+  const [message, setMessage] = useState("");
 
-    let updateStudents = async () => {
-        const token = localStorage.getItem('token') || "";
-        const response = await fetch("http://localhost:3001/getGroup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "x-access-token": token
-            },
-            body: JSON.stringify({id: id})
-        });
+  const updateStudents = async () => {
+    const token = localStorage.getItem('token') || "";
+    const response = await fetch("http://localhost:3001/getGroup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": token
+      },
+      body: JSON.stringify({ id: id })
+    });
 
-        if (response.ok) {
-            let data = await response.json();
-            setStudents(data.group.students || []);
-        }
+    if (response.ok) {
+      const data = await response.json();
+      setStudents(data.group.students || []);
     }
-    
-    let addStudent = async () => {
+  }
 
-        if (studentEmail === "") {
-            setMessageType("error");
-            setMessage("Please enter a student email!");
-        }
-        else {
-            const token = localStorage.getItem("token") || "";
-            const response = await fetch("http://localhost:3001/addStudent", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-access-token": token
-                },
-                body: JSON.stringify({groupId: id, userEmail: studentEmail})
-            });
+  const addStudent = async () => {
 
-            if (response.ok) {
-                if (studentEmailInput.current) {
-                    studentEmailInput.current.value = "";
-                }
-
-                setStudentEmail("");
-                updateStudents();
-                setMessageType("success");
-                setMessage("Student successfully added!");
-            }
-            else if (response.status == 400) {
-                const data = await response.json();
-                setMessageType(data.type);
-                setMessage(data.message);
-            }
-            else {
-                setMessageType("error");
-                setMessage("An error occurred :(");
-            }
-        }
+    if (studentEmail === "") {
+      setMessageType("error");
+      setMessage("Please enter a student email!");
     }
+    else {
+      const token = localStorage.getItem("token") || "";
+      const response = await fetch("http://localhost:3001/addStudent", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": token
+        },
+        body: JSON.stringify({ groupId: id, userEmail: studentEmail })
+      });
 
-    useEffect(() => {
+      if (response.ok) {
+        if (studentEmailInput.current) {
+          studentEmailInput.current.value = "";
+        }
+
+        setStudentEmail("");
         updateStudents();
-    }, []);
+        setMessageType("success");
+        setMessage("Student successfully added!");
+      }
+      else if (response.status == 400) {
+        const data = await response.json();
+        setMessageType(data.type);
+        setMessage(data.message);
+      }
+      else {
+        setMessageType("error");
+        setMessage("An error occurred :(");
+      }
+    }
+  }
 
-    return(
-        <div className="home">
-            <div className="container">
-                <div className='row border-bottom border-secondary pb-3 mb-3'>
-                    <div className='col-5'>
-                        Number of Students: {students.length}
-                    </div>
-                    <div className='col-7 text-end'>
-                        <button onClick={() => addStudent()}>Add Student</button>
-                        <input
-                            ref={studentEmailInput}
-                            placeholder="Student Email"
-                            onChange={(e) => {setStudentEmail(e.target.value)}}
-                            className={messageType == "error" ? "border border-danger" : ""}>
-                        </input>
-                        <br /> {message == "" ? null : <small className={messageType == "error" ? "text-danger" : "text-success"}>{message}</small>}
-                    </div>
-                </div>
-    
-                <div className='row'>
-                    {students.map((student) => 
-                        <div key={String(student._id)} className="col-12 mb-3">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">{student.firstName + " " + student.lastName}</h5>
-                                    <p className="card-text">
-                                        Overall Grade: {"TODO" /*TODO*/}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </div>
+  useEffect(() => {
+    updateStudents();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="container">
+        <div className='row border-bottom border-secondary pb-3 mb-3'>
+          <div className='col-5'>
+            Number of Students: {students.length}
+          </div>
+          <div className='col-7 text-end'>
+            <button onClick={() => addStudent()}>Add Student</button>
+            <input
+              ref={studentEmailInput}
+              placeholder="Student Email"
+              onChange={(e) => { setStudentEmail(e.target.value) }}
+              className={messageType == "error" ? "border border-danger" : ""}>
+            </input>
+            <br /> {message == "" ? null : <small className={messageType == "error" ? "text-danger" : "text-success"}>{message}</small>}
+          </div>
         </div>
-    );
+
+        <div className='row'>
+          {students.map((student) =>
+            <div key={String(student._id)} className="col-12 mb-3">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{student.firstName + " " + student.lastName}</h5>
+                  <p className="card-text">
+                    Overall Grade: {"TODO" /*TODO*/}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Group;
--- client/src/InstructorDashboard/Home.tsx
+++ client/src/InstructorDashboard/Home.tsx
@@ -1,104 +1,104 @@
-import {useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Home(){
-    const navigate = useNavigate();
-    const groupNameInput = useRef<HTMLInputElement>(null);
-    const [groupName, setGroupName] = useState("");
-    const [groups, setGroups] = useState<{_id: String, name: String, students: []}[]>([]);
-    const [messageType, setMessageType] = useState("");
-    const [message, setMessage] = useState("");
+function Home() {
+  const navigate = useNavigate();
+  const groupNameInput = useRef<HTMLInputElement>(null);
+  const [groupName, setGroupName] = useState("");
+  const [groups, setGroups] = useState<{ _id: string, name: string, students: [] }[]>([]);
+  const [messageType, setMessageType] = useState("");
+  const [message, setMessage] = useState("");
 
-    const createGroup = async () => {
+  const createGroup = async () => {
 
-        if (groupName === "") {
-            setMessageType("error");
-            setMessage("Please enter a team name!");
-        }
-        else {
-            const token = localStorage.getItem('token') || "";
-            const response = await fetch("http://localhost:3001/createGroup", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-access-token": token
-                },
-                body: JSON.stringify({name: groupName})
-            });
-
-            if (response.ok) {
-                if (groupNameInput.current) {
-                    groupNameInput.current.value = "";
-                }
-                
-                setGroupName("");
-                updateGroups();
-                setMessageType("success");
-                setMessage("Group creation success!");
-            }
-            else {
-                setMessageType("error");
-                setMessage("An error occurred :(");
-            }
-        }
+    if (groupName === "") {
+      setMessageType("error");
+      setMessage("Please enter a team name!");
     }
-    
-    const updateGroups = async () => {
-        const token = localStorage.getItem('token') || "";
-        const response = await fetch("http://localhost:3001/getGroups", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "x-access-token": token
-            }
-        });
+    else {
+      const token = localStorage.getItem('token') || "";
+      const response = await fetch("http://localhost:3001/createGroup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": token
+        },
+        body: JSON.stringify({ name: groupName })
+      });
 
-        if (response.ok) {
-            const data = await response.json();
-            setGroups(data.groups || []);
+      if (response.ok) {
+        if (groupNameInput.current) {
+          groupNameInput.current.value = "";
         }
-    }
-    
-    useEffect(() => {
+
+        setGroupName("");
         updateGroups();
-    }, []);
+        setMessageType("success");
+        setMessage("Group creation success!");
+      }
+      else {
+        setMessageType("error");
+        setMessage("An error occurred :(");
+      }
+    }
+  }
 
-    return(
-        <div className="home">
-            <div className="container">
-                <div className='row border-bottom border-secondary pb-3 mb-3'>
-                    <div className='col-5'>
-                        Number of Groups: {groups.length}
-                    </div>
-                    <div className='col-7 text-end'>
-                        <button onClick={() => createGroup()} className="mr-sm">Create Group</button>
-                        <input 
-                            ref={groupNameInput}
-                            placeholder="Group Name"
-                            onChange={(e) => {setGroupName(e.target.value)}}
-                            className={messageType == "error" ? "border border-danger" : ""}>
-                        </input>
-                        <br /> {message == "" ? null : <small className={messageType == "error" ? "text-danger" : "text-success"}>{message}</small>}
-                    </div>
-                </div>
-    
-                <div className='row'>
-                    {groups.map((group) => 
-                        <div key={String(group._id)} className="col-12 mb-3">
-                            <div className="card" onClick={() => navigate("/instructor/group/" + group._id)}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{group.name}</h5>
-                                    <p className="card-text">
-                                        Number of Students: {group.students.length}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </div>
+  const updateGroups = async () => {
+    const token = localStorage.getItem('token') || "";
+    const response = await fetch("http://localhost:3001/getGroups", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": token
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setGroups(data.groups || []);
+    }
+  }
+
+  useEffect(() => {
+    updateGroups();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="container">
+        <div className='row border-bottom border-secondary pb-3 mb-3'>
+          <div className='col-5'>
+            Number of Groups: {groups.length}
+          </div>
+          <div className='col-7 text-end'>
+            <button onClick={() => createGroup()} className="mr-sm">Create Group</button>
+            <input
+              ref={groupNameInput}
+              placeholder="Group Name"
+              onChange={(e) => { setGroupName(e.target.value) }}
+              className={messageType == "error" ? "border border-danger" : ""}>
+            </input>
+            <br /> {message == "" ? null : <small className={messageType == "error" ? "text-danger" : "text-success"}>{message}</small>}
+          </div>
         </div>
-    );
+
+        <div className='row'>
+          {groups.map((group) =>
+            <div key={String(group._id)} className="col-12 mb-3">
+              <div className="card" onClick={() => navigate("/instructor/group/" + group._id)}>
+                <div className="card-body">
+                  <h5 className="card-title">{group.name}</h5>
+                  <p className="card-text">
+                    Number of Students: {group.students.length}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
