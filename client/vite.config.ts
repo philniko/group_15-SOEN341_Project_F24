294a8c5f134851f98f0d7edf38e5a691ca1bb3a9
Ke-Li02 <143147612+Ke-Li02@users.noreply.github.com>
2024-11-27 18:13:24 -0500

    fixed ChatSidebar, DetailedView, and App

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig<string>({
   plugins: [react()],
   test: {
     globals: true,
