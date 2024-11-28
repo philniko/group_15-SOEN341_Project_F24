032563229ff81e06983bf639ee4625626d73b62b
Ke-Li02 <143147612+Ke-Li02@users.noreply.github.com>
2024-11-27 19:20:51 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig<string>({
+export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
